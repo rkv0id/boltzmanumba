2b300d3fa55d108eb9f5313a81e621d35061ddea
ramyk <ramy.kader.rk@gmail.com>
2021-03-04 11:27:05 +0100

    2x optimize runtime

--- lattice-gpu.py
+++ lattice-gpu.py
@@ -1,34 +1,29 @@
 import time as tm
 import numpy as np
 from math import sin, pi
-from numba import cuda, float64
+from numba import cuda
 from matplotlib.pyplot import imsave
 
 # Flow definition
 nx, ny      = 420, 180
-r           = ny // 9
-dims        = cuda.to_device([nx,ny])
 uLB         = 0.04
 Re          = 150.0
-nulb        = uLB*r/Re
+nulb        = uLB*(ny//9)/Re
 omega       = 1 / (3*nulb+0.5)
-consts      = cuda.to_device([uLB, omega])
 
-v = cuda.to_device(
-    np.array([
+v = cuda.to_device([
         [1,1], [1,0], [1,-1],
         [0,1], [0,0], [0,-1],
         [-1,1], [-1,0], [-1,-1]
-]))
-t = cuda.to_device(
-    np.array([
+])
+t = cuda.to_device([
         1/36, 1/9, 1/36,
         1/9, 4/9, 1/9,
         1/36, 1/9, 1/36
-]))
-col1 = cuda.to_device(np.array([0, 1, 2]))
-col2 = cuda.to_device(np.array([3, 4, 5]))
-col3 = cuda.to_device(np.array([6, 7, 8]))
+])
+col1 = np.array([0, 1, 2])
+col2 = np.array([3, 4, 5])
+col3 = np.array([6, 7, 8])
 
 # CUDA setup
 threadsperblock = (16, 8)
@@ -37,31 +32,31 @@ blockspergrid_y = int(np.ceil(ny / threadsperblock[1]))
 blockspergrid   = (blockspergrid_x, blockspergrid_y)
 
 @cuda.jit
-def initvel(out, uLB, nx, ny):
+def initvel(out):
     x, y = cuda.grid(2)
     if x < nx and y < ny:
         out[0,x,y] = uLB * (1 + 1e-4*sin(y/(ny-1)*2*pi))
         out[1,x,y] = 0
 
 @cuda.jit
-def equilibrium(out, rho, u, v, t, nx, ny):
+def equilibrium(out, rho, u, v, t):
     x, y = cuda.grid(2)
     if x < nx and y < ny:
         u0, u1 = u[0,x,y], u[1,x,y]
+        usqr = 3/2 * (u0**2 + u1**2)
         for i in range(9):
             cu = 3 * (v[i,0] * u0 + v[i,1] * u1)
-            out[i,x,y] = rho[x,y] * t[i] * \
-                (1 + cu + 0.5*cu*cu - 3/2 * (u0*u0 + u1*u1))
+            out[i,x,y] = rho[x,y] * t[i] * (1 + cu + 0.5*cu**2 - usqr)
 
 @cuda.jit
-def post_equilibrium(fin, feq, ny):
+def post_equilibrium(fin, feq):
     x, y = cuda.grid(2)
     if x == 0 and y < ny:
         for i in range(3):
             fin[i,x,y] = feq[i,x,y] + fin[8-i,x,y] - feq[8-i,x,y]
 
 @cuda.jit
-def rho_clc(out, fin, nx, ny):
+def rho_clc(out, fin):
     x, y = cuda.grid(2)
     if x < nx and y < ny:
         temp = 0
@@ -70,7 +65,7 @@ def rho_clc(out, fin, nx, ny):
         out[x,y] = temp
 
 @cuda.jit
-def macroscopic(out, fin, rho, v, nx, ny):
+def macroscopic(out, fin, rho, v):
     x, y = cuda.grid(2)
     if x < nx and y < ny:
         temp1, temp2 = 0, 0
@@ -81,47 +76,49 @@ def macroscopic(out, fin, rho, v, nx, ny):
         out[1,x,y] = temp2 / rho[x,y]
 
 @cuda.jit
-def outflow(fin, col3, nx, ny):
+def outflow(fin):
     x, y = cuda.grid(2)
     if x == nx-1 and y < ny:
         for i in col3:
             fin[i,x,y] = fin[i,x-1,y]
 
 @cuda.jit
-def inflow(u, rho, vel, fin, col2, col3, ny):
+def inflow(u, rho, vel, fin):
     x, y = cuda.grid(2)
     if x == 0 and y < ny:
         u[0,x,y] = vel[0,x,y]
         u[1,x,y] = vel[1,x,y]
         temp = 0
-        for c1,c2 in zip(col2,col3):
-            temp += fin[c1,x,y] + 2*fin[c2,x,y]
-        rho[x,y] = temp / (1-vel[0,x,y])
+        for c in col2:
+            temp += fin[c,x,y]
+        for c in col3:
+            temp += 2*fin[c,x,y]
+        rho[x,y] = 1/(1-vel[0,x,y]) * temp
 
 @cuda.jit
-def collision(out, fin, feq, omega, nx, ny):
+def collision(out, fin, feq):
     x, y = cuda.grid(2)
     if x < nx and y < ny:
         for i in range(9):
             out[i,x,y] = fin[i,x,y] - omega * (fin[i,x,y] - feq[i,x,y])
 
 @cuda.jit
-def bounce(fout, fin, nx, ny):
+def bounce(fout, fin):
     x, y = cuda.grid(2)
     if x < nx and y < ny:
         cx  = nx // 4
         cy  = ny // 2
         r   = ny // 9
-        if ((x-cx)*(x-cx) + (y-cy)*(y-cy)) < (r*r):
+        if ((x-cx)**2 + (y-cy)**2) < (r**2):
             for i in range(9):
                 fout[i,x,y] = fin[8-i,x,y]
 
 @cuda.jit
-def stream(fin, fout, v, nx, ny):
+def stream(fin, fout, v):
     x, y = cuda.grid(2)
     if x < nx and y < ny:
         for i in range(9):
-            fin[i,x,y] = fout[i, (x-v[i,0]) % nx, (y-v[i,1]) % ny]
+            fin[i,x,y] = fout[i, (x+v[i,0]) % nx, (y+v[i,1]) % ny]
 
 def main(maxIter, stateSv):
     print("Initializing Simulation...")
@@ -133,43 +130,43 @@ def main(maxIter, stateSv):
     u       = cuda.device_array((2,nx,ny))
 
     # init velocity field
-    initvel[blockspergrid, threadsperblock](vel, consts[0], dims[0], dims[1])
+    initvel[blockspergrid, threadsperblock](vel)
     # init equilibrium
-    equilibrium[blockspergrid, threadsperblock](fin, rho, vel, v, t, dims[0], dims[1])
+    equilibrium[blockspergrid, threadsperblock](fin, rho, vel, v, t)
 
     print("Starting Simulation...")
     figures = {}
     start = tm.time()
     for time in range(maxIter):
         # outflow conditions
-        outflow[blockspergrid, threadsperblock](fin, col3, dims[0], dims[1])
+        outflow[blockspergrid, threadsperblock](fin)
         
         # new Rho val
-        rho_clc[blockspergrid, threadsperblock](rho, fin, dims[0], dims[1])
+        rho_clc[blockspergrid, threadsperblock](rho, fin)
         
         # macro vel/density
-        macroscopic[blockspergrid, threadsperblock](u, fin, rho, v, dims[0], dims[1])
+        macroscopic[blockspergrid, threadsperblock](u, fin, rho, v)
         
         # inflow conditions
-        inflow[blockspergrid, threadsperblock](u, rho, vel, fin, col2, col3, dims[1])
+        inflow[blockspergrid, threadsperblock](u, rho, vel, fin)
         
         # equilibrium state re-compute
-        equilibrium[blockspergrid, threadsperblock](feq, rho, vel, v, t, dims[0], dims[1])
-        post_equilibrium[blockspergrid, threadsperblock](fin, feq, dims[1])
+        equilibrium[blockspergrid, threadsperblock](feq, rho, vel, v, t)
+        post_equilibrium[blockspergrid, threadsperblock](fin, feq)
         
         # collision compute
-        collision[blockspergrid, threadsperblock](fout, fin, feq, consts[1], dims[0], dims[1])
+        collision[blockspergrid, threadsperblock](fout, fin, feq)
         
         # obstacle bounce-back
-        bounce[blockspergrid, threadsperblock](fout, fin, dims[0], dims[1])
+        bounce[blockspergrid, threadsperblock](fout, fin)
         
         # streaming to next iter
-        stream[blockspergrid, threadsperblock](fin, fout, v, dims[0], dims[1])
+        stream[blockspergrid, threadsperblock](fin, fout, v)
         
         # Recording timestamp velocity
-        if (time % stateSv == 0):
+        if (time % stateSv == 2):
             fluid = u.copy_to_host()
-            fig = np.sqrt(fluid[0] ** 2 + fluid[1] ** 2).transpose() 
+            fig = np.sqrt(fluid[0]**2 + fluid[1]**2).transpose()
             figures[time//stateSv] = fig
     
     end = tm.time()
@@ -180,4 +177,4 @@ def main(maxIter, stateSv):
     print("DONE! Check ./gpu_out folder for progress visuals.")
 
 if __name__ == "__main__":
-    main(2000, 500)
+    main(20000, 1000)
